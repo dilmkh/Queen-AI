de3575454360e2fc5d6b3c82d37f9de027543615
dilmkh <124597615+dilmkh@users.noreply.github.com>
2023-06-02 07:56:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,15 +20,15 @@ const chalk = require('chalk');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-global.session = process.env.SESSION_ID || "PUT HERE"; //enter your session id here
+global.session = process.env.SESSION_ID || "queen~dGJDZXJrTDk="; //enter your session id here
 
-global.openaikey = process.env.OPEN_AI_KEY || "ENTER_API-KEY_HERE"; //Put Your Open AI Api key
+global.openaikey = process.env.OPEN_AI_KEY || "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"; //Put Your Open AI Api key
 
-global.ownername = process.env.OWNER_NAME || "DarkWinzo"; //Queen-AI Owner Name here
+global.ownername = process.env.OWNER_NAME || "DARK-SHADOW"; //Queen-AI Owner Name here
 
-global.ownernumber = process.env.OWNER_NUMBER || "94775200935"; //Owner whatsapp number here
+global.ownernumber = process.env.OWNER_NUMBER || "94751150234"; //Owner whatsapp number here
 
-global.language = process.env.LANGUAGE || "EN"; //Enter bot working language EN = English / SI = Sinhala /ML = Malayalam /HI =Hindi /UR = Urdu
+global.language = process.env.LANGUAGE || "SI"; //Enter bot working language EN = English / SI = Sinhala /ML = Malayalam /HI =Hindi /UR = Urdu
 
 global.prefix = ['.'] //Don't Change
 
